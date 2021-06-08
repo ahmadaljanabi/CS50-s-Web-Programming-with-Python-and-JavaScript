e69e147c9264e217bd8071a41738e41a1a5af444
احمد الجنبي <ahmad-3434@hotmail.com>
2021-06-08 14:00:11 +0300

    new file

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Welcome to Python Programming")
+
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
